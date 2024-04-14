7de4edc98c2b628437e22acd7ea907a22f17fc07
Mayur P Patil <ai.mayurpatil@gmail.com>
2024-04-14 20:11:43 +0530

    Add Save Operation Done

--- WPF_MVVM_EF6_MSSQL/WPF_MVVM_EF6_MSSQL/ViewModels/EmployeeViewModel.cs
+++ WPF_MVVM_EF6_MSSQL/WPF_MVVM_EF6_MSSQL/ViewModels/EmployeeViewModel.cs
@@ -5,35 +5,94 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using WPF_MVVM_EF6_MSSQL.Models;
+using WPF_MVVM_EF6_MSSQL.Commands;
+using System.Collections.ObjectModel;
 
 namespace WPF_MVVM_EF6_MSSQL.ViewModels
 {
     public class EmployeeViewModel : INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged_Implemention
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string propertyName)
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
 
         EmployeeService ObjEmployeeService;
         public EmployeeViewModel()
         {
             ObjEmployeeService = new EmployeeService();
             LoadData();
+            CurrentEmployee = new Employee();
+            saveCommand = new RelayCommand(Save);
+            
         }
-        private List<Employee> employeeList;
 
-        public List<Employee> EmployeeList
+        #region Display_Operation
+        private ObservableCollection<Employee> employeeList;
+
+        public ObservableCollection<Employee> EmployeeList
         {
             get { return employeeList; }
             set { employeeList = value; OnPropertyChanged("EmployeeList"); }
         }
         private void LoadData()
         {
-            EmployeeList = ObjEmployeeService.GetAll();
+            EmployeeList =new ObservableCollection<Employee>( ObjEmployeeService.GetAll());
         }
+        #endregion
+
+        private Employee currentEmployee;
+
+        public Employee CurrentEmployee
+        {
+            get { return currentEmployee; }
+            set { currentEmployee = value; OnPropertyChanged("CurrentEmployee"); }
+        }
+
+
+
+        #region SaveOperation
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged("Message"); }
+        }
+        private RelayCommand saveCommand;
+
+        public RelayCommand SaveCommand
+        {
+            get { return saveCommand; }
+            
+        }
+
+        public void Save()
+        {
+            try
+            {
+                var IsSaved = ObjEmployeeService.Add(CurrentEmployee);
+                LoadData();
+                if (IsSaved)
+                    Message = "Employee saved";
+                else
+                    Message = "Saved operation failed";
+
+            }
+            catch (Exception ex)
+            {
+
+                Message = ex.Message;
+            }
+        }
+        #endregion
+
+
+
 
     }
 }
